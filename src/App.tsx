b3cebcafed08eeeece69267768f059d32a407f4b
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-06-19 11:45:23 -0400

    mobile try 8

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ import Contact from './components/Contact/Contact';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Epk from './components/Epk/Epk';
+import {SafeAreaView} from 'react-native';
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={ <Home />} />
-          <Route path="about" element={ <About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="epk" element={<Epk />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-      </Routes>
-      </BrowserRouter>
-
+      <SafeAreaView>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={ <Home />} />
+            <Route path="about" element={ <About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="epk" element={<Epk />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+        </BrowserRouter>
+      </SafeAreaView>
     </div>
   );
 }
