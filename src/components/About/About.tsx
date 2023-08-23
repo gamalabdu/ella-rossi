1735cad8ef7ec87f3c196236b82e347e52da4902
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-22 21:34:26 -0400

    keep trying gamal

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
 	return (
 		<div className='about-container'>
 
-			<img className='video' src={cantYouTell} style={{ filter: "brightness(60%)" }} />
+			<img className='video' src={cantYouTell} style={{ filter: "brightness(60%)", WebkitFilter: "brightness(80%)" }} />
 
 			<div className='about-container-2'>
 
