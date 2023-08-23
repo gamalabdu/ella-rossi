2185b18070ea3d392e1141c51abd619f26b9b4fe
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-08-22 20:48:55 -0400

    darkened the about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
 	return (
 		<div className='about-container'>
 
-			<img className='video' src={cantYouTell} />
+			<img className='video' src={cantYouTell} style={{ filter: "brightness(60%)" }} />
 
 			<div className='about-container-2'>
 
