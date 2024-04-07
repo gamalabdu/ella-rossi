1b56dfe3a0a09bb8abf8ba35b105c76cfac80a56
Gamal Abdu <abdu.s.gamal@gmail.com>
2024-04-07 19:06:31 -0400

    adding new home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect,lazy, Suspense } from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
 import './styles.css'
 import linenVideo from '../../assets/videos/linen.mp4'
-import { FaInstagram, FaTiktok } from "react-icons/fa6";
+import { FaInstagram, FaTiktok } from 'react-icons/fa6'
 import { AiOutlineYoutube } from 'react-icons/ai'
 import { TbBrandSpotify } from 'react-icons/tb'
 import './styles.css'
-import christmas from '../../assets/pictures/christmastime.png'
+// import christmas from '../../assets/pictures/christmastime.png'
 import front from '../../assets/front.png'
 import back from '../../assets/back.png'
-
+import frontHeartEyes from "../../assets/pictures/HeartEyesOfficialCoverArt.jpeg"
+import frontHeartBack from "../../assets/pictures/HeartEyesOfficialBackCoverArt.jpeg"
 
 function Home() {
-
 	const isabellaInfo = {
 		name: 'Ella Rossi',
-		latest:"https://open.spotify.com/track/5Jy3tQAgMdDz7z9Hj25Ha6?si=bcaadfb456d14e66",
+		latest:
+			'https://open.spotify.com/track/5Jy3tQAgMdDz7z9Hj25Ha6?si=bcaadfb456d14e66',
 		instagram: 'https://www.instagram.com/ellarossimusic/',
 		youtube: 'https://www.youtube.com/@EllaRossi',
-		spotify: 'https://open.spotify.com/artist/5XeDL6Pow4Ag5j9HWv9KHm?si=FJ0XHLupQu-nCVfFfwni9w',
+		spotify:
+			'https://open.spotify.com/artist/5XeDL6Pow4Ag5j9HWv9KHm?si=FJ0XHLupQu-nCVfFfwni9w',
 		tiktok: 'https://www.tiktok.com/@ellarossimusic',
 	}
-
-
 
 	const goToLink = (linkType: string) => {
 		switch (linkType) {
@@ -34,19 +34,17 @@ function Home() {
 			case 'latest':
 				return (window.location.href = `${isabellaInfo.latest}`)
 			case 'tiktok':
-					return (window.location.href = `${isabellaInfo.tiktok}`)
+				return (window.location.href = `${isabellaInfo.tiktok}`)
 			default:
 				break
 		}
 	}
 
-
-
 	return (
 		<div className='home-container'>
 			{/* <video src={linenVideo} autoPlay loop muted playsInline={true} disablePictureInPicture={true} /> */}
 
-			{/* <div className='flip-card'>
+			<div className='flip-card'>
 				<div className='flip-card-inner'>
 					<div className='flip-card-front'>
 					    <img className='front' src={front} style={{ height:"100%" , width:"100%"}} />
@@ -55,25 +53,45 @@ function Home() {
 						Welcome 
 					</div>
 				</div>
-			</div> */}
+			</div>
 
-           <h1 className='header-title'>ELLA ROSSI</h1>
+			<h1 className='header-title'>ELLA ROSSI</h1>
 
-
-
-
-			<div className='latest-cover-art-container'>
+			{/* <div className='latest-cover-art-container'>
 				<div className='latest-text'>
-					"Christmas Time Is Here" available in all streaming platforms{' '}
+					"Heart Eyes" available in all streaming platforms{' '}
 				</div>
 				<div className='latest-cover-art-wrapper'>
 					<img
 						className='latest-cover-art'
-						src={christmas}
+						src={frontHeartEyes}
 						onClick={() => goToLink('latest')}
 					/>
 				</div>
+			</div> */}
+
+
+
+
+          <div className='latest-cover-art-container'>
+
+		  <div className='latest-text'>
+					"Heart Eyes" available in all streaming platforms{' '}
+		</div>
+
+		  <div className='flip-card-art'>
+				<div className='flip-card-inner'>
+					<div className='flip-card-front'>
+					    <img className='front' src={frontHeartEyes} style={{ height:"100%" , width:"100%"}} />
+					</div>
+					<div className='flip-card-back' style={{ height:"100%" , width:"100%", display:"flex", justifyContent:"center", alignItems:"center", textAlign:"center"}} >
+					    <img className='front' src={frontHeartBack} style={{ height:"100%" , width:"100%"}} />
+					</div>
+				</div>
 			</div>
+				
+			</div>
+
 
 
 			<div className='artistSocialsContainer'>
@@ -87,11 +105,9 @@ function Home() {
 					<TbBrandSpotify />
 				</button>
 				<button className='artistSocials' onClick={() => goToLink('tiktok')}>
-					<FaTiktok/>
+					<FaTiktok />
 				</button>
 			</div>
-
-			
 		</div>
 	)
 }
